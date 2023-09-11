c6e1f2c8ca1b748711d545acb9e0145d76a5d6c6
everton.koga <everton.koga@tmov.com.br>
2023-09-11 10:36:20 -0300

    adicionado springboot na camada de application

--- infrastructure/src/main/java/br/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/br/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,9 +1,13 @@
 package br.fullcycle.admin.catalogo.infrastructure;
 
-import br.fullcycle.admin.catalogo.application.UseCase;
+import br.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new UseCase().execute());
+        System.out.println("oi");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/br/fullcycle/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package br.fullcycle.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.fullcycle.admin.catalogo")
+public class WebServerConfig {
+}
