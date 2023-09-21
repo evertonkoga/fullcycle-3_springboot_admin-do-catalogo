e38845e710b6a79c85941757a1db51e0efad29ed
everton.koga <everton.koga@tmov.com.br>
2023-09-21 11:08:14 -0300

    Implementado estrutura para testes integrados com banco de dados

---
+++ infrastructure/src/main/java/br/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -0,0 +1,46 @@
+package br.fullcycle.admin.catalogo.infrastructure.category;
+
+import br.fullcycle.admin.catalogo.domain.category.Category;
+import br.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import br.fullcycle.admin.catalogo.domain.category.CategoryID;
+import br.fullcycle.admin.catalogo.domain.category.CategorySearchQuery;
+import br.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import br.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryMySQLGateway implements CategoryGateway {
+
+    private final CategoryRepository repository;
+
+    public CategoryMySQLGateway(final CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public void deleteBy(CategoryID anId) {
+
+    }
+
+    @Override
+    public Optional<Category> findBy(CategoryID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+}
